86848287ddd8de8f54dd4cb0da5abdc1bc5efefb
Jacob Enders <jacobenders1@gmail.com>
2022-08-31 13:56:42 -0400

    test azure deployment

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React HELLO AZURE</h1>
+      <h2>Heyyyy Jacob, PUSH TO MASTER</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
